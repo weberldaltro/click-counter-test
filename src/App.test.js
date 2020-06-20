dba40dcc90db9666e9dd3d52002fedf8fcfb0dde
Weber Lopes Daltro <weber.daltro@ab-inbev.com>
2020-06-20 15:04:31 -0300

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test("renders increment button", () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find("[data-test='increment-button']");
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test("counter starts at 0", () => {});
+
+test("clicking button increments counter display", () => {});
